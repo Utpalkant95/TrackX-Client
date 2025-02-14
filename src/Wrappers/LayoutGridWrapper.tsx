449eaaa1e9f9f6fd6ceb78e55f2c864bc0cb741a
Utpal Kant <utpalkant881@gmail.com>
2025-02-14 23:04:18 +0530

    bug fix

--- src/Wrappers/LayoutGridWrapper.tsx
+++ src/Wrappers/LayoutGridWrapper.tsx
@@ -1,5 +1,5 @@
 const LayoutGridWrapper = ({ children, Cols=2 } : { children: React.ReactNode , Cols?: number}) => {
-  const gridClass = `grid gap-8 md:grid-cols-2`
+  const gridClass = `grid gap-8 md:grid-cols-2   ht-${Cols}`
   return (
     <div className={gridClass}>
       {children}
