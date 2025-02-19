ece89f88f8fcc3a8598885d31d57b6b30abcd5a5
Utpal Kant <utpalkant881@gmail.com>
2025-02-19 17:45:34 +0530

    hook is maded

---
+++ src/hooks/useUserSettings.ts
@@ -0,0 +1,47 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  workoutReminder: {
+    workoutReminder: false,
+    reminderTime: "",
+  },
+  progessAiAlerts: {
+    plateauAlerts: false,
+    goalTrackingAlerts: false,
+  },
+  emailNotifications: {
+    receiveWeeklyProgressReports: false,
+    receiveSpecialTrainingTipsUpdates: false,
+  },
+};
+
+const validationSchema = Yup.object().shape({
+  workoutReminder: Yup.object().shape({
+    workoutReminder: Yup.boolean(),
+    reminderTime: Yup.string(),
+  }),
+  progessAiAlerts: Yup.object().shape({
+    plateauAlerts: Yup.boolean(),
+    goalTrackingAlerts: Yup.boolean(),
+  }),
+  emailNotifications: Yup.object().shape({
+    receiveWeeklyProgressReports: Yup.boolean(),
+    receiveSpecialTrainingTipsUpdates: Yup.boolean(),
+  }),
+});
+
+const useUserSettings = () => {
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return {
+    formik,
+  };
+};
+
+export default useUserSettings;
