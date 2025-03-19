5564a8110ae690cfd3c53220931d78f0a084dc7a
Utpal Kant <utpalkant881@gmail.com>
2025-03-19 16:42:13 +0530

    done

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -24,7 +24,7 @@ const useLogin = () => {
     onSuccess: (data: IRES) => {
       formik.resetForm();
       enqueueSnackbar(data.message, { variant: "success" });
-      // window.location.reload();
+      window.location.reload();
       navigate("/");
     },
     onError: (error: AxiosError<IRES>) => {
